6ca4e054f86cbb92d88c93c55613ca8b6457af3f
Himanshurai3721 <166324191+Himanshurai3721@users.noreply.github.com>
2026-04-23 21:21:25 +0530

    Update1.3 app.py

--- app.py
+++ app.py
@@ -1413,6 +1413,6 @@ def serve(path):
     return send_from_directory(app.static_folder, "index.html")
 
 if __name__ == "__main__":
-    
-app.run(host="0.0.0.0",port=port,debug=True)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host="0.0.0.0", port=port, debug=True)
 
